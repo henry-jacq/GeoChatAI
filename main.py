fa3be5715cd4a84a834ede8da31738212957f128
henry-jacq <henryeditz267@gmail.com>
2025-03-08 15:42:15 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/api/upload")
+def upload():
+    return jsonify({
+        "status": True
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True)
